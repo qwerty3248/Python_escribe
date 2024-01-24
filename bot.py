6f366ba726b9881356cf7ab20f7261fb124745bb
qwerty3248 <jesuslosada4484@gmail.com>
2024-01-24 08:54:31 +0100

    Bueno aqui esta todo pero nada funciona

---
+++ bot.py
@@ -0,0 +1,14 @@
+import pywhatkit
+from datetime import datetime
+import time
+
+numero_tel = "+34660770992"
+mensaje = "Como estás?"
+
+# Obtener la hora actual + 60 segundos
+seconds = time.time() + 60
+date = datetime.fromtimestamp(seconds)
+
+# Enviar mensaje con pywhatkit
+pywhatkit.sendwhatmsg(numero_tel, mensaje, date.hour, date.minute)
+	
